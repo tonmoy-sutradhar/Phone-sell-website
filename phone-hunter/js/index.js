350fd53c59aa2ce18663ccefbb4d9f82d812f45f
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-10-05 13:37:58 +0600

    Partial e done.

--- phone-hunter/js/index.js
+++ phone-hunter/js/index.js
@@ -35,7 +35,7 @@ const displayAllPhones = (phones) => {
       <h2 class="card-title">${brand}</h2>
       <p>${slug}</p>
       <div class="card-actions">
-      <button class="btn btn-primary">Show Details</button>
+      <button onclick= "phoneDetails('${slug}')" class="btn btn-primary">Show Details</button>
        </div>
      </div>
     </div>
@@ -55,6 +55,34 @@ const handleSearch = () => {
 
 const handleShowAll = () => {
   loadAllPhones(true);
+};
+
+const phoneDetails = async (slugs) => {
+  // console.log(slug);
+  const res = await fetch(
+    ` https://openapi.programming-hero.com/api/phone/${slugs}`
+  );
+  const data = await res.json();
+  console.log(data.data);
+
+  const { brand, image, slug } = data.data;
+
+  const modalContainer = document.getElementById("modal-container");
+  modalContainer.innerHTML = `
+   <dialog id="my_modal_1" class="modal">
+          <div class="modal-box">
+            <h3 class="text-lg font-bold">${brand}!</h3>
+            <p class="py-4">Press ESC key or click the button below to close</p>
+            <div class="modal-action">
+              <form method="dialog">
+                <!-- if there is a button in form, it will close the modal -->
+                <button class="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+  `;
+  my_modal_1.showModal();
 };
 
 loadAllPhones(false, "iphone");
